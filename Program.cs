80fad3cd4c51ba4c3b84cb6696ebfde9f906f68a
GrafanyS <admin@valet.of.by>
2022-08-05 10:55:23 +0300

    Comments

--- Program.cs
+++ Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] arrayone = new string[11] { "hello", "2", "world", ":-)", "1234", "1567", "-2", "computer science", "Russia", "Denmark", "Kazan" };
+string[] arraytwo = new string[arrayone.Length];
+void ArrayWith(string[] arrayone, string[] arraytwo)// метод вывбора >3 элементов
+{
+    int count = 0;
+    for (int i = 0; i < arrayone.Length; i++)
+    {
+        if (arrayone[i].Length <= 3)
+        {
+            arraytwo[count] = arrayone[i];
+            count++;
+        }
+    }
+}
+void PrintArray(string[] array)// метод печати
+{
+    Console.WriteLine("-Результат");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($" {array[i]} ");
+    }
+
+}
+ArrayWith(arrayone, arraytwo);
+PrintArray(arraytwo);
